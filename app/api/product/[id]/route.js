1e4c86d504dea974655847554f077d06b1723d8f
Guitar <jilamikalalae@gmail.com>
2024-09-20 14:31:36 +0700

    add pagination

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -1,11 +1,45 @@
 import Product from "@/models/Product";
 
-export async function GET(request, { params }) {
-  const id = params.id;
-  const product = await Product.findById(id).populate("category");
-  console.log({ product });
+// export async function GET(request, { params }) {
+//   const id = params.id;
+//   const product = await Product.findById(id).populate("category");
+//   console.log({ product });
+//   return Response.json(product);
+// }
+
+export async function GET(request) {
+  // console.log('GET /api/category',request.nextUrl.searchParams.get("pno"))
+
+  const pno = request.nextUrl.searchParams.get("pno");
+
+  if (pno) {
+    const size = 3;
+
+    const startIndex = (pno - 1) * size;
+
+    const product = await Category.find()
+      .sort({ order: -1 })
+      .skip(startIndex)
+      .limit(size);
+
+    return Response.json(product);
+  }
+
+  const s = request.nextUrl.searchParams.get("s");
+
+  if (s) {
+    const product = await Category.find({
+      name: { $regex: s, $options: "i" },
+    }).sort({ order: -1 });
+
+    return Response.json(product);
+  }
+
+  const product = await Category.find().sort({ order: -1 });
+
   return Response.json(product);
 }
+
 
 export async function DELETE(request, { params }) {
   const id = params.id;
